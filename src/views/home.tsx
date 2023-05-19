d3e5316cf4a25cade87e598fa9ad4f9ff710853c
Luis Montiel <83305447+LuisM03@users.noreply.github.com>
2023-05-19 12:29:56 -0500

    Ultimos cambios

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -17,7 +17,7 @@ export default function Home(){
   }
 
   return (
-    <div className='flex flex-col items-center justify-center w-full min-h-screen px-4'>
+    <div className='flex flex-col items-center justify-center w-full min-h-screen px-4 my-10'>
       <div className='flex flex-col items-center gap-10'>
         <div>
           <h1 className='text-center font-bold text-slate-900 text-7xl'>Serpientes y Escaleras</h1>
